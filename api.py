8eee90eed21da76fe7eaf72a8770aec17ddb9afb
Dev-Zz <zz-dev@outlook.com>
2022-06-20 15:56:34 -0300

    update

--- api.py
+++ api.py
@@ -6,21 +6,23 @@ from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium import webdriver
 from time import sleep
 
-#options = FirefoxOptions()
-#options.add_argument("--headless")
-#browser = webdriver.Firefox(options=options)
-
-browser = Firefox()
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route('/consulta-cep')
 def my_route():
+    options = FirefoxOptions()
+    options.add_argument("--headless")
+    browser = webdriver.Firefox(options=options)
+    #browser = Firefox()
     url = 'http://cep.republicavirtual.com.br/web_cep.php?cep='
     cep = request.args.get('cep')
     url = url + cep + '&formato=json'
     browser.get(url)
-    browser.page_source
-    
+    browser_page_source = browser.page_source
+    browser.quit() 
+    return browser_page_source
+  
+
 app.run()
